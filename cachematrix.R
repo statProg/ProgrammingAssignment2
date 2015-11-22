74f0f35d9c75092825068614801902d0e717fd06
statProg <andreas.mai@nagler-company.com>
2015-11-22 20:52:38 +0100

    Update cachematrix.R
    Added description

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,6 @@
+
+## These functions calcualte the inverse matrix and retrieve the calculated matrix
+## if the matrix was calculated previously.
 makeCacheMatrix <- function(matrix = matrix()) {
   inverseMatrix<-NULL
   
@@ -16,7 +19,9 @@ makeCacheMatrix <- function(matrix = matrix()) {
        setInverseMatrix=setInverseMatrix,
        getInverseMatrix=getInverseMatrix)
 }
-
+## This function checks if the respective matrix is stored in cache. if yes and the value of the respective
+## matrix is in the cache, the cached data is returned. If the matrix was not calculated previously, the matrix is
+## calculated (the inverse of the matrix).
 cacheSolve <- function(matrix=matrix(), ...) {
   inverseMatrix<-matrix$getInverseMatrix()
   if(!is.null(inverseMatrix)){
