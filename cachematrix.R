7ebea2ac623a6ef9e8e3b88a0cb14a9a5a6ae92b
statProg <andreas.mai@nagler-company.com>
2015-11-22 21:58:11 +0100

    Update cachematrix.R
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,27 @@
-## These functions calcualte the inverse matrix and retrieve the calculated matrix
-## if the matrix was calculated previously.
+## This code has two functions. The first function (makeCacheMatrix) is able to 
+## cache the inverse of an nxn Matrix. 
+## The second function(cacheSolve) is able to return the inverse of matrix 
+## from cache if the matrix is in cache.
+## If the matrix is not in cache, the inverse is (re-)calculated. 
+
 makeCacheMatrix <- function(matrix = matrix()) {
   inverseMatrix<-NULL
+  
+## clear cache and set new matrix  
   
   set<-function(y){
     matrix<<-y
     inverseMatrix<<-NULL
   }
   
+## get matrix  
   get<-function() matrix
-  
+
+## save matrix (in cache)
+ 
   setInverseMatrix<-function(z) inverseMatrix<<- z
+
+## get inverse Matrix from cache
   
   getInverseMatrix<-function() inverseMatrix
   
@@ -21,13 +32,19 @@ makeCacheMatrix <- function(matrix = matrix()) {
 ## This function checks if the respective matrix is stored in cache. if yes and the value of the respective
 ## matrix is in the cache, the cached data is returned. If the inverse matrix was not calculated previously, the matrix is
 ## calculated.
+
 cacheSolve <- function(matrix=matrix(), ...) {
-  inverseMatrix<-matrix$getInverseMatrix()
+ 
+## check cache for inverse matrix (read and check)  
+  
+   inverseMatrix<-matrix$getInverseMatrix()
   if(!is.null(inverseMatrix)){
     print("get cached data...")
     return(inverseMatrix)
   }
   
+## If inverse matrix is not in cache, it is recalculated. 
+   
   else {
     print("Calculate inverse matrix...")
     calcMatrix<-matrix$get()
